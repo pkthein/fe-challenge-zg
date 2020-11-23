cfc153b5a9d40cde05d9c6fde411b3d16e39cbb9
Phyo Htut <pkthein@gmail.com>
2020-11-22 22:50:03 -0800

    making sure clicking on thumbnails selects the image data

--- src/component/PhotoGallery/index.js
+++ src/component/PhotoGallery/index.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState }  from 'react'
 import PropTypes from 'prop-types'
 import './index.css'
 
 const PhotoGallery = ({ data }) => {
+  const [image, setImage] = useState({ url: '', caption: '', index: 0 })
+  const [visible, setVisible] = useState(false)
+
+  useEffect(() => {
+    console.log(image, visible)
+  }, [image, visible])
+
+  const displayImage = (idx) => {
+    setImage({ ...data[idx], index: idx })
+    setVisible(true)
+  }
+
   return (
     <div align="center">
       <h4 className="photo-gallery-text-h4" >Photo Gallery</h4>
@@ -14,8 +26,17 @@ const PhotoGallery = ({ data }) => {
           <img
             className="photo-gallery-image-box" width="300"
             key={ idx } alt={ img.caption } src={ img.url }
+            onClick={ e => displayImage(idx) }
           />
         ))
+      }
+
+      {
+        visible && (image.url && image.caption) && (
+          <div className="">
+            Selected Image
+          </div>
+        )
       }
     </div>
   )
@@ -25,7 +46,13 @@ PhotoGallery.propTypes = {
   data: PropTypes.arrayOf(PropTypes.shape({
     url: PropTypes.string,
     caption: PropTypes.string
-  }))
+  })),
+  image: PropTypes.shape({
+    url: PropTypes.string,
+    caption: PropTypes.string,
+    index: PropTypes.number
+  }),
+  visible: PropTypes.bool
 }
 
 export default PhotoGallery
