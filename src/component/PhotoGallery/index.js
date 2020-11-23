7a462b7dc11f1a3a4c49ef61234ea3c41d434379
Phyo Htut <pkthein@gmail.com>
2020-11-22 22:38:33 -0800

    displaying the thumbnails for the user to choose from

--- src/component/PhotoGallery/index.js
+++ src/component/PhotoGallery/index.js
@@ -3,9 +3,22 @@ import PropTypes from 'prop-types'
 import './index.css'
 
 const PhotoGallery = ({ data }) => {
-  console.log(data)
+  return (
+    <div align="center">
+      <h4 className="photo-gallery-text-h4" >Photo Gallery</h4>
+      <hr/>
 
-  return (<div>Welcome from PhotoGallery</div>)
+      {
+        data.length > 0 &&
+        data.map((img, idx) => (
+          <img
+            className="photo-gallery-image-box" width="300"
+            key={ idx } alt={ img.caption } src={ img.url }
+          />
+        ))
+      }
+    </div>
+  )
 }
 
 PhotoGallery.propTypes = {
